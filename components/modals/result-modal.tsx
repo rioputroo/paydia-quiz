b74c87f1e81db865b6fe52bd052fbc14c98e6162
Rio Trilaksono Putro <riotrilaksonop@gmail.com>
2024-07-22 13:58:35 +0700

    feat: update result modal

--- components/modals/result-modal.tsx
+++ components/modals/result-modal.tsx
@@ -10,6 +10,7 @@ import {
 import { Separator } from "@/components/ui/separator";
 import useModalStore from "@/hooks/useModalStore";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const ResultModal = () => {
   const { isOpen, type, onClose, additionalData } = useModalStore();
@@ -25,16 +26,47 @@ const ResultModal = () => {
           </DialogTitle>
         </DialogHeader>
         <Separator />
-        <div className="flex flex-col items-center py-4 md:py-6">
-          <p className="text-lg md:2xl text-primary font-semibold tracking-wide">
-            You scored: {`${additionalData?.score}/${additionalData?.limit}`}
+
+        {additionalData?.score === additionalData?.limit &&
+          <div className="flex justify-center items-center">
+            <Image
+              src={"/quiz-check.svg"}
+              alt="quiz check"
+              priority
+              width={48}
+              height={48}
+            />
+          </div>
+        }
+
+        {additionalData?.score !== additionalData?.limit &&
+          <div className="flex justify-center items-center">
+            <Image
+              src={"/quiz-cross.svg"}
+              alt="quiz cross"
+              priority
+              width={48}
+              height={48}
+            />
+          </div>
+        }
+        <div className="flex flex-col items-center">
+          <p className="text-[20px] md:2xl text-primary font-semibold tracking-wide">
+            <p className="text-[20px] font-medium text-[#090A0B]">You scored:</p>
+            {additionalData?.score === additionalData?.limit &&
+              <span className="flex justify-center items-center text-4xl text-[#0B7B69]">{`${additionalData?.score}/${additionalData?.limit}`}</span>
+            }
+
+            {additionalData?.score !== additionalData?.limit &&
+              <span className="flex justify-center items-center text-4xl text-[#C03744]">{`${additionalData?.score}/${additionalData?.limit}`}</span>
+            }
           </p>
           <Button
             onClick={() => {
               router.push("/");
               onClose();
             }}
-            className="mt-3 md:mt-5"
+            className="mt-3 md:mt-5 bg-[#1926b2] text-white w-[138px] h-[44px] text-base hover:bg-[#1926b2]"
           >
             Play Again
           </Button>
