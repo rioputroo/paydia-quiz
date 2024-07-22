a6d5948eef26e0afa5a7231023a98072c42027bc
Rio Trilaksono Putro <riotrilaksonop@gmail.com>
2024-07-22 14:58:22 +0700

    feat: added PWA

---
+++ app/offline/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Offline = () => {
+    return (
+        <div>Halaman yang kamu cari tidak ada</div>
+    )
+}
+
+export default Offline
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+import withPWA from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const pwaConfig = {
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    dest: "public",
+    fallbacks: {
+        //image: "/static/images/fallback.png",
+        document: "/offline", // if you want to fallback to a custom page rather than /_offline
+        // font: '/static/font/fallback.woff2',
+        // audio: ...,
+        // video: ...,
+    },
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+    // ... other options you like
+};
+
+export default withPWA(pwaConfig)(nextConfig);
